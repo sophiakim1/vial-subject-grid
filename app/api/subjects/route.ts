e475b2648b5e097f73a12398e6ad022cace30a9f
Sophia Kim <sophiakim.dev@gmail.com>
2024-08-09 18:09:51 -0400

    Add Subject type to the project, add \api\subjects route that returns mock data, and use the mock API data in the grid

---
+++ app/api/subjects/route.ts
@@ -0,0 +1,64 @@
+import { NextResponse } from "next/server";
+
+/**
+ * Sleep function to mock API latency
+ * @param ms time in milliseconds
+ * @returns Promise<void>
+ */
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * GET /api/subjects
+ * Returns a list of subjects after waiting for 0.1 seconds
+ * @returns Promise<void>
+ */
+export async function GET() {
+  // Simulate API latency
+  await sleep(100);
+  return NextResponse.json(subjects);
+}
+
+const subjects = [
+  {
+    id: 1,
+    name: "Sophia Kim",
+    age: 28,
+    gender: "F",
+    diagnosisDate: "2024-08-09",
+    status: "Pending",
+  },
+  {
+    id: 2,
+    name: "Dixie Elvin",
+    age: 28,
+    gender: "M",
+    diagnosisDate: "2024-07-10",
+    status: "Approved",
+  },
+  {
+    id: 3,
+    name: "Winton Navy",
+    age: 28,
+    gender: "M",
+    diagnosisDate: "2024-04-11",
+    status: "In Progress",
+  },
+  {
+    id: 4,
+    name: "Scout Caleb",
+    age: 28,
+    gender: "M",
+    diagnosisDate: "2023-12-12",
+    status: "Released",
+  },
+  {
+    id: 5,
+    name: "Fleur Sally",
+    age: 28,
+    gender: "F",
+    diagnosisDate: "2023-11-15",
+    status: "Released",
+  },
+];
---
+++ types.d.ts
@@ -0,0 +1,8 @@
+type Subject = {
+  id: number;
+  name: string
+  age: number
+  gender: string // TODO: Use enum
+  diagnosisDate: string // TODO: Use Date type
+  status: string // TODO: Use enum
+}
