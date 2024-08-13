d8cf1d4f27ebee4af1752ac73432cf543407389f
sophia <sophiakim.dev@gmail.com>
2024-08-13 04:45:40 -0400

    Finalizing about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,13 +2,11 @@
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import HeaderSection from "../components/navigation/HeaderSection";
+import Link from "next/link";
 import {
-    Container,
     Title,
     List,
     ThemeIcon,
-    Group,
-    Divider,
     Grid,
     Card,
     Image,
@@ -22,43 +20,43 @@ export default function About() {
         <div className={styles.aboutContainer}>
             <HeaderSection />
             <div className={styles.content}>
-            <h1 className={styles.subject}>Project Details</h1>
-            <p>
+            <Title order={1} mb="15px">Project Details</Title>
+            <p className={styles.description}>
                 This is a Next.js project designed to provide a user-friendly
                 interface for displaying and managing subject data. Built with
                 modern technologies such as React, TypeScript, and Mantine.
             </p>
             </div>
 
-            <div>
-            <Title order={3} mb="sm">
+            <div className={styles.content}>
+            <Title order={2} mb="15px">
                 Technologies
             </Title>
             <List
                 spacing="sm"
                 size="md"
                 center
-                icon={<ThemeIcon color="teal" size={14} radius="xl"></ThemeIcon>}
+                icon={<ThemeIcon color="#c3dddd" size={12} radius="xl" aria-hidden="true"></ThemeIcon>}
+                className={styles.list}
             >
                 <List.Item>Next.js</List.Item>
                 <List.Item>React</List.Item>
                 <List.Item>TypeScript</List.Item>
                 <List.Item>Mantine</List.Item>
-                <List.Item>PostCSS</List.Item>
             </List>
             </div>
 
-            <div>
-            <Title order={3} mb="sm">
+            <div className={styles.content}>
+            <Title order={2} mb="15px">
                 Features
             </Title>
             <Grid>
             {[
                 { src: "/image/ProjectBoard.png", text: "Project Board" },
                 { src: "/image/responsive.png", text: "Responsive Design" },
-                { src: "/image/filter.png", text: "Filter & Search Options" },
-                { src: "/image/sort.png", text: "Sorting Features" },
-                { src: "/image/ProjectBoard", text: "Display a grid of subjects with their details" },
+                { src: "/image/Filter.png", text: "Filter & Search Options" },
+                { src: "/image/Sort.png", text: "Sorting Features" },
+                { src: "/image/scroll.png", text: "Infinite scroll" },
                 { src: "/image/test.png", text: "Unit Testing" },
             ].map((item, index) => (
                 <Grid.Col span={4} key={index}>
@@ -68,7 +66,7 @@ export default function About() {
                         src={item.src}
                         height={400}
                         width={50}
-                        alt={`Image ${index + 1}`}
+                        alt={item.text}
                     />
                     </Card.Section>
                     <Text mt="md">{item.text}</Text>
@@ -77,6 +75,10 @@ export default function About() {
             ))}
             </Grid>
             </div>
+                    
+            <footer>
+                <Link href="https://www.sophiakim.ca/" className={styles.footer}>Made by Sophia Kim</Link>
+            </footer>
         </div>
     );
 }
